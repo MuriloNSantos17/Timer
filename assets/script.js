78d51b7e9ded8f4ff7dc2db1fad16a36e282f9fd
MuriloDSantos <77493959+MuriloDSantos@users.noreply.github.com>
2021-09-27 17:35:06 -0300

    Commit Final

---
+++ assets/script.js
@@ -0,0 +1,61 @@
+let segundos=0;
+let timer = document.querySelector('.timer');
+var botaoStart = document.querySelector('.start');
+var botaoStop = document.querySelector('.stop');
+var botaoReset = document.querySelector('.reset');
+
+let relogio;
+
+function iniciar()
+{
+    clearInterval(relogio);
+
+    relogio = setInterval(()=>{
+
+        let data = new Date(segundos*1000).toLocaleTimeString('pt-BR',{
+            timeZone: 'UTC'
+        });
+
+        console.log(data);
+        segundos++;
+
+        timer.innerHTML=data;
+   
+
+    },1000);
+}
+
+
+function pausar(){
+    
+    clearInterval(relogio);
+}
+
+
+function resetar()
+{
+    clearInterval(relogio);
+    timer.innerHTML="00:00:00";
+    segundos=0;
+}
+
+
+
+
+
+botaoStart.addEventListener('click',()=>{
+    iniciar();
+});
+
+botaoStop.addEventListener('click',()=>{
+    pausar();
+});
+
+botaoReset.addEventListener('click',()=>{
+    resetar();
+});
+
+
+
+
+
